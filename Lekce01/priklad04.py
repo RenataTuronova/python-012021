27c0cded1f5dca259a6337f75a0528f7b4f3563f
RenataTuronova <renataturonova1@gmail.com>
2021-02-28 18:00:22 +0100

    Hotové úkoly lekce 1

---
+++ Lekce01/priklad04.py
@@ -0,0 +1,73 @@
+# program04 - Morseova abeceda
+#
+# Ve slovníku níže vidíš Morseovu abecedu, kde jako klíč slouží znak v klasické abecedě a jako hodnota zápis znak v Morseově abecedě.
+
+morseCode = {
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+    ".": ".-.-.-",
+    ",": "--..--",
+    "?": "..--..",
+    "!": "-.-.--",
+    "-": "-....-",
+    "/": "-..-.",
+    "@": ".--.-.",
+    "(": "-.--.",
+    ")": "-.--.-"
+}
+# 1. Napiš program, který se uživatele zeptá na text, který chce zapsat v Morseově abecedě. Uvažuj disciplinovaného uživatele, který zadává pouze znaky bez diakritiky, malá písmena atd. Na začátku uvažuj i to, že uživatel nezadává mezery.
+# 2. Projdi řetězec zadaný uživatelem. Najdi každý znak ve slovníku a vypiš ho na obrazovku v Morseově abecedě.
+
+text = input("Zadejte text, který chcete zapsat v Morseově abecedě: ")
+for znak in text:
+  print(morseCode[znak])
+
+
+# 3. Abychom měli celý kód vypsaný na jedné řádce, požádáme funkci print(), aby na konci výpisu nevkládala znak pro konec řádku, ale mezeru. To uděláme tak, že jako druhý arugument funkce dáme argument end=" ".
+
+text = input("Zadejte text, který chcete zapsat v Morseově abecedě: ")
+for znak in text:
+  print(morseCode[znak], end = " ")
+
+
+# 4. Nyní přidáme mezery. Uvažuj, že uživatel může zadat mezeru. Před tím, než budeš hledat znak ve slovníku, zkontroluj, zda znak není mezera. Pokud ano, vypiš znak lomítka /.
+
+text = input("Zadejte text, který chcete zapsat v Morseově abecedě: ")
+morseCode[" "] = "/"
+for znak in text:
+  print(morseCode[znak], end=" ")
+
