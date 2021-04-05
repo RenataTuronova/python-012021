2936a074d5a2ea5fe7f6163abb88ed7494fdaa1e
RenataTuronova <renataturonova1@gmail.com>
2021-04-05 23:06:48 +0200

    Oprava úkolu č. 18 #4

--- Lekce04/priklad18.py
+++ Lekce04/priklad18.py
@@ -11,35 +11,36 @@
 
 from datetime import datetime
 
-now = datetime.now()
-dateNow = now.strftime("%d. %m. %Y")
-
 class Contact:
   def __init__(self, name, email):
     self.name = name
     self.email = email
 
+
 class Candidate(Contact):
-  def __init__(self, name, email, dateOfInterview, recordFromInterview = ""):
+  def __init__(self, name, email, dateOfInterview, recordFromInterview=""):
     super().__init__(name, email)
     self.dateOfInterview = dateOfInterview
     self.recordFromInterview = recordFromInterview
 
   def saveRecord(self, recordFromInterview):
-    self.recordFromInterview = recordFromInterview
-    if self.dateOfInterview < dateNow:
+    dateNow = datetime.now()
+    interviewDate = datetime.strptime(self.dateOfInterview, "%d.%m.%Y")
+    if interviewDate < dateNow:
+      self.recordFromInterview = recordFromInterview
       return f"Zápis z pohovoru byl uložen."
     else:
       return f"Chybné zadání. Je potřeba zadat již proběhlý pohovor."
 
-
-
-candidate1 = Candidate("David Kopec", "davidkopec@gmail.com", "15. 3. 2021")
-candidate2 = Candidate("Eva Volná", "evavolna@gmail.com", "16. 3. 2021")
-candidate3 = Candidate("Dorota Malá", "dorotamala@gmail.com", "17. 3. 2021")
+candidate1 = Candidate("David Kopec", "davidkopec@gmail.com", "2.4.2021")
+candidate2 = Candidate("Eva Volná", "evavolna@gmail.com", "10.4.2021")
+candidate3 = Candidate("Dorota Malá", "dorotamala@gmail.com", "10.3.2021")
 
 print(f"Kontaktní údaje uchazeče: {candidate1.name}, email: {candidate1.email}, datum pohovoru: {candidate1.dateOfInterview}")
 print(candidate1.saveRecord("Kandidát byl u pohovoru úspěšný."))
 
-print(f"Kontaktni udaje uchazece: {candidate2.name}, email: {candidate2.email}, datum pohovoru: {candidate2.dateOfInterview}")
-print(candidate2.saveRecord("Kandidat byl u pohovoru uspesny."))
+print(f"Kontaktní údaje uchazeče: {candidate2.name}, email: {candidate2.email}, datum pohovoru: {candidate2.dateOfInterview}")
+print(candidate2.saveRecord("Kandidát byl u pohovoru úspěšný."))
+
+print(f"Kontaktní údaje uchazeče: {candidate3.name}, email: {candidate3.email}, datum pohovoru: {candidate3.dateOfInterview}")
+print(candidate3.saveRecord("Kandidát byl u pohovoru úspěšný."))
