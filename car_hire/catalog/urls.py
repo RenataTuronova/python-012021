d6d0ef86bcbe5bc47688f1d2fdfbddfe33c47feb
RenataTuronova <79136199+RenataTuronova@users.noreply.github.com>
2021-04-17 22:15:11 +0200

    Delete car_hire/catalog directory

--- car_hire/catalog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('list/', views.ListView.as_view(), name='list')
-]
